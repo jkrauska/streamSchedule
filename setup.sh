674a035ec12e77f37564964cd90d684426d723c7
Joel Krauska <jkrauska@gmail.com>
2024-05-15 22:28:30 -0700

    qpt

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Needed before uwsgi
+sudo apt-get install libssl-dev
+sudo apt-get install libpcre3 libpcre3-dev
+
+
 [ ! -d ".venv" ] && python -m venv .venv
 
 source .venv/bin/activate
